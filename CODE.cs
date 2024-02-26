39ea914a10fe77e2f8f13e77576a83e125189426
OldM1lk <144899273+OldM1lk@users.noreply.github.com>
2024-02-26 17:26:22 +0700

    Update CODE.cs
    Создаk класс текстового файла с возможностью бинарной и
    XML сериализации/десериализации.

--- CODE.cs
+++ CODE.cs
@@ -1,1 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
+namespace StandartInputOutput {
+  [Serializable]
+  class TextFile {
+    public string fileName;
+    public string content;
+
+    public TextFile(string fileName, string content) {
+      this.fileName = fileName;
+      this.content = content;
+    }
+
+    public void BinarySerialize(FileStream fileStream) {
+      BinaryFormatter binaryFormatter = new BinaryFormatter();
+      binaryFormatter.Serialize(fileStream, this);
+      fileStream.Flush();
+      fileStream.Close();
+    }
+
+    public void BinaryDeserialize(FileStream fileStream) {
+      BinaryFormatter binaryFormatter = new BinaryFormatter();
+      TextFile deserialized = (TextFile)binaryFormatter.Deserialize(fileStream);
+      fileName = deserialized.fileName;
+      content = deserialized.content;
+      fileStream.Close();
+    }
+
+    public void XMLSerialize(FileStream fileStream) {
+      XmlSerializer xmlSerializer = new XmlSerializer(this.GetType());
+      xmlSerializer.Serialize(fileStream, this);
+      fileStream.Flush();
+      fileStream.Close();
+    }
+
+    public void XMLDeserialize(FileStream fileStream) {
+      XmlSerializer xmlSerializer = new XmlSerializer(this.GetType());
+      TextFile deserialized = (TextFile)xmlSerializer.Deserialize(fileStream);
+      fileName = deserialized.fileName;
+      content = deserialized.content;
+      fileStream.Close();
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+
+    }
+  }
+}
